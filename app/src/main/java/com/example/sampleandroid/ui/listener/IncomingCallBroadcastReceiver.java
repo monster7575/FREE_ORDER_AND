008048184052789bd9b43a1586e7ed783d7a3767
kwon7575 <kwon7575@nzonesoft.co.kr>
2018-04-27 20:57:44 +0900

    SMS 전송 전처리 로직 추가

--- app/src/main/java/com/example/sampleandroid/ui/listener/IncomingCallBroadcastReceiver.java
+++ app/src/main/java/com/example/sampleandroid/ui/listener/IncomingCallBroadcastReceiver.java
@@ -12,8 +12,19 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.example.sampleandroid.common.preference.BasePreference;
+import com.example.sampleandroid.common.tool.Logger;
+import com.example.sampleandroid.common.tool.Utils;
+import com.example.sampleandroid.data.model.BuyerReponse;
+import com.example.sampleandroid.data.model.SellerData;
+import com.example.sampleandroid.data.model.SellerReponse;
+import com.example.sampleandroid.data.model.SellerVO;
 import com.example.sampleandroid.data.service.CallingService;
+import com.example.sampleandroid.data.tool.DataInterface;
+import com.example.sampleandroid.data.tool.DataManager;
+import com.example.sampleandroid.ui.activity.MainActivity;
 
+import java.util.HashMap;
 
 
 /**
@@ -28,10 +39,10 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver{
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
 
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-
+        Log.e("before  : ", state);
         if(state.equals(mLastState))
         {
             return;
@@ -40,15 +51,40 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver{
         {
             mLastState = state;
         }
-
+        Log.e("EXTRA_STATE_RINGING  : ", "" + TelephonyManager.EXTRA_STATE_RINGING);
         if(TelephonyManager.EXTRA_STATE_RINGING.equals(state))
         {
+
+            SellerVO sellerVO = BasePreference.getInstance(context).getObject(BasePreference.SELLER_DATA, SellerVO.class);
+            final String idx = String.valueOf(sellerVO.getIdx());
+            final String content = sellerVO.getContent();
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            final String phoneNumber = PhoneNumberUtils.formatNumber(incomingNumber);
+            final String phoneNumber = PhoneNumberUtils.formatNumber(incomingNumber).replace("-", "");
             Log.e("phoneNumber : ", phoneNumber);
-            Intent serviceIntent = new Intent(context, CallingService.class);
-            serviceIntent.putExtra(CallingService.EXTRA_CALL_NUMBER, phoneNumber);
-            context.startService(serviceIntent);
+
+            HashMap<String, String> params = new HashMap<>();
+            params.put("phonenb", phoneNumber);
+            DataManager.getInstance(context).api.getBuyer(context, params, new DataInterface.ResponseCallback<BuyerReponse>() {
+                @Override
+                public void onSuccess(BuyerReponse response) {
+
+                    Log.e("getBuyer  : ", "" + Utils.getStringByObject(response));
+
+                    String bobjid = String.valueOf(response.data.get(0).getIdx());
+
+                    Intent serviceIntent = new Intent(context, CallingService.class);
+                    serviceIntent.putExtra(CallingService.EXTRA_CALL_NUMBER, phoneNumber.replace("-", ""));
+                    serviceIntent.putExtra(CallingService.EXTRA_SELLER_IDX, idx);
+                    serviceIntent.putExtra(CallingService.EXTRA_SELLER_CONTENT, content);
+                    serviceIntent.putExtra(CallingService.EXTRA_BUYER_IDX, bobjid);
+                    context.startService(serviceIntent);
+                }
+
+                @Override
+                public void onError() {
+                    Logger.log(Logger.LogState.E, "savelog fail");
+                }
+            });
         }
     }
 }
